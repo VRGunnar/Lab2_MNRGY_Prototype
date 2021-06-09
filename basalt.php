7e4e129a83c5dc70cb701cb960ff3b264e2609b3
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-06-09 14:12:40 +0200

    HTML & CSS of Basalt.php finished for the biggest part

---
+++ basalt.php
@@ -0,0 +1,175 @@
+<?php
+include_once("partials/nav.inc.php");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/css.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet">
+    <title>View your basalt info!</title>
+</head>
+<body class="px-4 background4">
+<a href="dashboard.php">
+    <svg class="mt-4" xmlns="http://www.w3.org/2000/svg" width="23.482" height="17.219" viewBox="0 0 23.482 17.219">
+        <defs>
+            <style>
+                .cls-1 {
+                    fill: #f8f8ff;
+                }
+            </style>
+        </defs>
+        <g id="fast-forward" transform="translate(23.482 81.232) rotate(180)">
+            <g id="Group_22" data-name="Group 22" transform="translate(10.175 64.013)">
+                <g id="Group_21" data-name="Group 21">
+                    <path id="Path_294" data-name="Path 294" class="cls-1"
+                          d="M221.106,72.1l-7.044-7.827a.783.783,0,0,0-.582-.26h-4.7a.782.782,0,0,0-.582,1.306l6.573,7.3-6.573,7.3a.783.783,0,0,0,.582,1.307h4.7a.793.793,0,0,0,.582-.258l7.044-7.827A.783.783,0,0,0,221.106,72.1Z"
+                          transform="translate(-208 -64.013)"/>
+                </g>
+            </g>
+            <g id="Group_24" data-name="Group 24" transform="translate(0 64.013)">
+                <g id="Group_23" data-name="Group 23" transform="translate(0)">
+                    <path id="Path_295" data-name="Path 295" class="cls-1"
+                          d="M13.106,72.1,6.062,64.273a.783.783,0,0,0-.582-.26H.784A.782.782,0,0,0,.2,65.319l6.573,7.3L.2,79.925a.783.783,0,0,0,.582,1.307h4.7a.793.793,0,0,0,.582-.258l7.044-7.827A.783.783,0,0,0,13.106,72.1Z"
+                          transform="translate(0 -64.013)"/>
+                </g>
+            </g>
+        </g>
+    </svg>
+</a>
+<div class="mt-4 mb-6">
+    <h2 class="font-extralight text-2xl">View your info of</h2>
+    <h1 class="font-bold text-2xl mb-2">Basalt</h1>
+    <span class="line-w bg-green h-0.5 block self-center "></span>
+</div>
+<div class="grid grid-cols-2 grid-rows-2 justify-items-stretch">
+    <svg class="col-start-1 cold-end-2 row-start-1 row-end-3" xmlns="http://www.w3.org/2000/svg" width="156.26"
+         height="87.059" viewBox="0 0 156.26 87.059">
+        <defs>
+            <style>
+                .cls-11 {
+                    fill: #1dd05d;
+                }
+
+                .cls-2 {
+                    fill: #fff;
+                }
+            </style>
+        </defs>
+        <g id="Group_94" data-name="Group 94" transform="translate(-36 -180.052)">
+            <path id="batteryFill" class="cls-11"
+                  d="M56.26,17.412V69.948h-38.9V17.412ZM138.9,56.588h8.681V30.471H138.9V10.882a2.1,2.1,0,0,0-2.17-2.176H10.851a2.1,2.1,0,0,0-1.558.614,2.143,2.143,0,0,0-.612,1.563V76.177a2.1,2.1,0,0,0,2.17,2.176H136.727a2.1,2.1,0,0,0,1.558-.614,2.143,2.143,0,0,0,.612-1.563ZM156.26,30.471V56.588a8.7,8.7,0,0,1-8.681,8.706V76.177a10.893,10.893,0,0,1-10.851,10.882H10.851a10.445,10.445,0,0,1-7.665-3.2A10.473,10.473,0,0,1,0,76.177V10.882A10.5,10.5,0,0,1,3.186,3.2,10.413,10.413,0,0,1,10.851,0H136.727a10.445,10.445,0,0,1,7.665,3.2,10.473,10.473,0,0,1,3.186,7.687V21.765a8.7,8.7,0,0,1,8.681,8.706Z"
+                  transform="translate(36 180.052)"/>
+            <rect id="Rectangle_1080" data-name="Rectangle 1080" class="cls-2" width="3.16" height="52.5"
+                  transform="translate(53.36 197.5)"/>
+        </g>
+    </svg>
+    <p class="text-black font-semibold text-3xl col-start-2 cold-end-3 row-start-1 row-end-2 justify-self-end">
+        Charged</p>
+    <p class="text-white pl-5 font-bold text-4xl col-start-2 cold-end-3 row-start-2 row-end-3 justify-self-left">33%</p>
+</div>
+
+<div class="w-full glassmorphism mt-8">
+        <div class="flex flex-row items-center mt-3">
+            <svg class="ml-5 mr-5" xmlns="http://www.w3.org/2000/svg" width="30" height="35" viewBox="0 0 24 29">
+                <defs>
+                    <style>
+                        .cls-1 {
+                            fill: #f8f8ff;
+                        }
+                    </style>
+                </defs>
+                <g id="Group_16" data-name="Group 16" transform="translate(-446.045 -95.17)">
+                    <path id="Path_286" data-name="Path 286" class="cls-1"
+                          d="M533.689,97.208a6.987,6.987,0,0,0,0,9.837,6.873,6.873,0,0,0,9.769,0,6.988,6.988,0,0,0,0-9.837,6.873,6.873,0,0,0-9.769,0Z"
+                          transform="translate(-80.711)"/>
+                    <path id="Path_287" data-name="Path 287" class="cls-1"
+                          d="M468.626,352.081a4.805,4.805,0,0,0,1.419-3.628c0-.582-.019-1.156-.058-1.706a17.18,17.18,0,0,0-.233-1.824,14.294,14.294,0,0,0-.447-1.835,9.067,9.067,0,0,0-.751-1.711,6.453,6.453,0,0,0-1.133-1.482,5,5,0,0,0-1.627-1.027,5.606,5.606,0,0,0-2.078-.377,2.1,2.1,0,0,0-1.126.479c-.337.221-.732.477-1.173.76a6.731,6.731,0,0,1-1.518.671,5.876,5.876,0,0,1-3.713,0,6.72,6.72,0,0,1-1.517-.671c-.436-.28-.831-.536-1.174-.76a2.1,2.1,0,0,0-1.126-.479,5.59,5.59,0,0,0-2.077.378,4.99,4.99,0,0,0-1.628,1.026,6.446,6.446,0,0,0-1.133,1.482,9.109,9.109,0,0,0-.752,1.711,14.491,14.491,0,0,0-.447,1.835,17.117,17.117,0,0,0-.233,1.826c-.038.552-.058,1.126-.058,1.705a4.806,4.806,0,0,0,1.419,3.628,5.1,5.1,0,0,0,3.656,1.339h13.851A5.1,5.1,0,0,0,468.626,352.081Z"
+                          transform="translate(0 -229.25)"/>
+                </g>
+            </svg>
+            <h2 class="font-medium text-2xl">Your battery info</h2>
+        </div>
+    <div class="flex flex-col items-end mt-0.5">
+        <span class="line-w2 bg-red h-0.5 block self-right"></span>
+    </div>
+    <div class="grid grid-cols-2 grid-rows-2 mt-2 pl-5 pr-5 justify-items-stretch">
+        <p class="col-start-1 col-end-3 row-start-1 row-end-2 justify-self-start text-white font-medium text-base">Available to charge up</p>
+        <p class="col-start-2 col-end-3 row-start-1 row-end-2 justify-self-end text-white font-medium text-base">15% / 100%</p>
+        <div class="w-full bg-white col-start-1 col-end-3 row-start-2 row-end-3 h-2 rounded-full mt-2">
+            <div class="w-3/4 bg-green h-2 rounded-full"></div>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 grid-rows-2 mt-2 pl-5 pr-5 justify-items-stretch mb-2">
+        <p class="col-start-1 col-end-3 row-start-1 row-end-2 justify-self-start text-white font-medium text-base">Current energy stored</p>
+        <p class="col-start-2 col-end-3 row-start-1 row-end-2 justify-self-end text-white font-medium text-base">2.5% / 15%</p>
+        <div class="w-full bg-white col-start-1 col-end-3 row-start-2 row-end-3 h-2 rounded-full mt-2">
+            <div class="w-3/4 bg-green h-2 rounded-full"></div>
+        </div>
+    </div>
+</div>
+
+<div class="w-full glassmorphism mt-8">
+    <div class="flex flex-row items-center mt-3">
+        <svg class="ml-5 mr-5" id="statistics"
+             xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 111 111">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #f8f8ff;
+                    }
+                </style>
+            </defs>
+            <g id="Group_11" data-name="Group 11" transform="translate(0 43.607)">
+                <g id="Group_10" data-name="Group 10">
+                    <path id="Path_281" data-name="Path 281" class="cls-1"
+                          d="M27.75,187.733H3.964A3.964,3.964,0,0,0,0,191.7v59.464a3.964,3.964,0,0,0,3.964,3.964H27.75a3.964,3.964,0,0,0,3.964-3.964V191.7A3.965,3.965,0,0,0,27.75,187.733ZM23.786,247.2H7.928V195.662H23.786Z"
+                          transform="translate(0 -187.733)"/>
+                </g>
+            </g>
+            <g id="Group_13" data-name="Group 13" transform="translate(39.643)">
+                <g id="Group_12" data-name="Group 12">
+                    <path id="Path_282" data-name="Path 282" class="cls-1"
+                          d="M198.416,0H174.63a3.964,3.964,0,0,0-3.964,3.964V107.035A3.964,3.964,0,0,0,174.63,111h23.786a3.964,3.964,0,0,0,3.964-3.964V3.964A3.964,3.964,0,0,0,198.416,0Zm-3.964,103.071H178.595V7.928h15.857Z"
+                          transform="translate(-170.666)"/>
+                </g>
+            </g>
+            <g id="Group_15" data-name="Group 15" transform="translate(79.285 31.714)">
+                <g id="Group_14" data-name="Group 14">
+                    <path id="Path_283" data-name="Path 283" class="cls-1"
+                          d="M369.083,136.533H345.3a3.964,3.964,0,0,0-3.964,3.964v71.357a3.964,3.964,0,0,0,3.964,3.964h23.786a3.964,3.964,0,0,0,3.964-3.964V140.5A3.964,3.964,0,0,0,369.083,136.533Zm-3.964,71.357H349.261V144.462h15.857Z"
+                          transform="translate(-341.333 -136.533)"/>
+                </g>
+            </g>
+        </svg>
+        <h2 class="font-medium text-2xl">Want to charge up?</h2>
+    </div>
+
+    <div class="flex flex-col items-end mt-0.5">
+        <span class="line-w2 bg-green h-0.5 block self-right"></span>
+    </div>
+    <div class="flex flex-col items-center mt-2 px-4">
+        <p class="text-white font-medium text-base mb-4 text-center">Average energy generation today: <br><span class="text-green text-2xl">7.6 kWh</span></p>
+        <div class="bg-white rounded-full w-full flex flex-row justify-between p-1 mb-4">
+            <button class="bg-green rounded-full px-4 text-white font-medium text-base">CHARGE</button>
+            <button class="rounded-full text-black font-medium text-base">CONSUME</button>
+            <button class="rounded-full text-black font-medium text-base mr-4">BALANCED</button>
+        </div>
+        <div class="w-full bg-green col-start-1 col-end-3 row-start-2 row-end-3 h-2 rounded-full">
+            <div class="w-3/4 bg-green h-2 rounded-full"></div>
+        </div>
+        <p class="font-base text-white text-center my-2">100% of the generated energy will be used to charge up the battery.</p>
+    </div>
+
+</div>
+
+</body>
+</html>
